3b66e117e349c170fd13082d3f9b51e0e843e015
Nikki Little <nikkilittle13@gmail.com>
2023-09-14 12:21:43 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+return drivers.slice(0,2);
+}
+
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+console.log(returnLastTwoDrivers(drivers));
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) { function fareMultiplier(fare) {
+    return fare * multiplier;
+}
+    return fareMultiplier;
+}
+
+const fareDoubler = function(fare) {
+    const doubler = createFareMultiplier(2);
+    return doubler(fare);
+}
+
+const fareTripler = function(fare) {
+    const tripler = createFareMultiplier(3);
+    return tripler(fare);
+}
+
+function selectDifferentDrivers(driversArray, driversFunction) {
+    return driversFunction(driversArray);
+}
